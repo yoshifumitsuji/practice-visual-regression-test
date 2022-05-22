3bc550926052b6d690dd872bba46b7182ee8eda2
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-05-22 17:02:50 +0900

    Practice visual regression test

--- jest/test/sample.test.ts
+++ jest/test/sample.test.ts
@@ -1,35 +1,37 @@
-const path = require('path');
-const fs = require('fs');
-
 describe('Sample test', () => {
   beforeEach(async () => {
-    // テスト用WEBサイトにアクセスする
     await page.goto('http://localhost:8000');
   });
 
-  it('title = test', async () => {
-    // テスト用WEBサイトのテストページのタイトルを確認する
-    const title = await page.title();
-    expect(title).toBe('test');
+  it('screenshot', async () => {
+    // ページのスクリーンショットを保存
+    const image = await page.screenshot();
+    // スクリーンショットと画像スナップショットを比較
+    expect(image).toMatchImageSnapshot();
   });
+
+  // it('type test', async () => {
+  //   const inputText = 'hoge';
+  //   await page.type('#txt', inputText);
+  //   const actual = await page.$eval('input[id="txt"]', (el) => (el as HTMLInputElement).value);
+  //   expect(actual).toBe(inputText);
+
+  //   // ページのスクリーンショットを保存
+  //   const image = await page.screenshot();
+  //   // スクリーンショットと画像スナップショットを比較
+  //   expect(image).toMatchImageSnapshot();
+  // });
 
   it('type test', async () => {
-    const inputText = 'hoge';
-
-    // idがtxtのDOMに対して'hoge'を入力する
+    // hoge -> hogeeee
+    const inputText = 'hogeeee';
     await page.type('#txt', inputText);
-
-    // idがtxtのinputタグの入力値を取得する
     const actual = await page.$eval('input[id="txt"]', (el) => (el as HTMLInputElement).value);
     expect(actual).toBe(inputText);
+
+    // ページのスクリーンショットを保存
+    const image = await page.screenshot();
+    // スクリーンショットと画像スナップショットを比較
+    expect(image).toMatchImageSnapshot();
   });
-});
-
-it('test1.png ok', () => {
-  // test/test.pngを読み込む
-  const imagePath = path.join(process.cwd(), 'test/test.png');
-  const file = fs.readFileSync(imagePath);
-
-  // 読み込んだ画像データを比較
-  expect(file).toMatchImageSnapshot();
 });
